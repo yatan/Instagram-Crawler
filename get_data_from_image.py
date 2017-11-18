cfb91a7d9da4426cdf3f8d50cd1b2c0a14bcdf0e
yatan <yatan87@gmail.com>
2017-11-19 00:46:31 +0100

    get_data_refactor

--- get_data_from_image.py
+++ get_data_from_image.py
@@ -4,26 +4,41 @@ import os, sqlite3
 
 db = sqlite3.connect('./web/data/crawler')
 
+def check_existed_link_id(link_id):
+    cursor = db.cursor()
+    cursor.execute('SELECT COUNT(*) FROM data WHERE link_id="' + link_id + '"')
+    if cursor.fetchone()[0] == 0:
+        return False
+    else:
+        return True
+
+def get_API(photo_url):
+    app = ClarifaiApp(api_key='f607cf42c8b64ff499931bdf6a5a53a6')
+    # get the general model
+    model = app.models.get("general-v1.3")
+    # predict with the model
+    response = model.predict_by_url(url=photo_url)
+
+    concepts = response['outputs'][0]['data']['concepts']
+    return concepts
+
+'''     for concept in concepts:
+        print(concept['name'], concept['value']) '''
 
 cursor = db.cursor()
 cursor.execute('SELECT * FROM links')
 for row in cursor:
     # row['name'] returns the name column in the query, row['email'] returns email column.
-    print "Geting: " + str(row[0]) + " " + row[2]
+    print "Geting info id: " + str(row[0]) + " url: " + row[2]
 
     #print('Getting info link id {0} = {1}'.format(row["id"], row["link"]))
-
-    url1 = row[2]
-
-    app = ClarifaiApp(api_key='f607cf42c8b64ff499931bdf6a5a53a6')
-    # get the general model
-    model = app.models.get("general-v1.3")
-    # predict with the model
-    response = model.predict_by_url(url=url1)
-
-    concepts = response['outputs'][0]['data']['concepts']
+    # Get API info to DB
+    # for concept in concepts:
+    #    print(concept['name'], concept['value'])
+    concepts = get_API(row[2])
     for concept in concepts:
         print(concept['name'], concept['value'])
+
 
 db.close()
 
