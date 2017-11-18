c9cf0676d3d20b682c18a1ae32209686bb927cef
yatan <yatan87@gmail.com>
2017-11-18 15:48:50 +0100

    thread in

--- crawler.py
+++ crawler.py
@@ -1,10 +1,9 @@
 import urllib2
 import sys
 # import re
-import threading
-
+from threading import Thread
 from bs4 import BeautifulSoup
-#from BeautifulSoup import BeautifulSoup
+
 
 # GET user value from argument or using 'default'
 active_user = "twittersoz"
@@ -12,58 +11,57 @@ if len(sys.argv) == 2:
     active_user = sys.argv[1]
 
 
-# Thread worker for active_user
-def worker(active_user):
-    print "Analizyng user: " + active_user
+class Crawler(Thread):
+    users = []
+    scanned_users = []
+    file_users = file
+
+    def __init__(self, active_user):
+        Thread.__init__(self)
+        print("In __init__")
+
+    # Thread worker for active_user
+    def run (self):
+        print "Analizyng user: " + active_user
+
+        # Parse active user
+        response = urllib2.urlopen('https://www.instagram.com/' + active_user)
+        html = response.read()
+        soup = BeautifulSoup(html, "html.parser")
+
+        self.file_users = open("users_" + active_user + ".txt","w") 
+
+        self.parse_users(soup)
+    def parse_users(self, soup):
+        for coses in soup.find_all('a'):
+            #print coses.get('a')
+            #nicks = coses.find_all('a')
+            conv = str(coses)
+            for tag in conv.split('>'):
+                if "@" in tag:
+                    tag2 = tag.split(' ')
+                    for element in tag2:
+                        if "@" in element:
+                            usuari = element.replace("@", "")
+                            if usuari not in self.users:
+                                self.users.append(usuari)
+                                self.file_users.write(usuari + "\n")
+                            #if re.match(r'.*[\%\$\^\*\@\!\_\-\(\)\:\;\'\"\{\}\[\]].*', element):
+                            #if set('[~!@#$%^&*()_+{}":;\']+$').intersection(element):
+                            #print element
+        self.file_users.close()
+        print self.users
 
 
-    
 
-    return
 
 # Llista threads actius
-threads = list()
-
-t = threading.Thread(target=worker, args=(active_user,))
-threads.append(t)
-t.start()
-
-# Parse active user
-response = urllib2.urlopen('https://www.instagram.com/' + active_user)
-html = response.read()
-soup = BeautifulSoup(html, "html.parser")
+e = Crawler(active_user)
+e.start()
 
 
-users = []
-scanned_users = []
 
-
-file_users = open("users_" + active_user + ".txt","w") 
-
-
-def parse_users(soup):
-    for coses in soup.find_all('a'):
-        #print coses.get('a')
-        #nicks = coses.find_all('a')
-        conv = str(coses)
-        for tag in conv.split('>'):
-            if "@" in tag:
-                tag2 = tag.split(' ')
-                for element in tag2:
-                    if "@" in element:
-                        usuari = element.replace("@", "")
-                        if usuari not in users:
-                            users.append(usuari)
-                            file_users.write(usuari + "\n")
-                        #if re.match(r'.*[\%\$\^\*\@\!\_\-\(\)\:\;\'\"\{\}\[\]].*', element):
-                        #if set('[~!@#$%^&*()_+{}":;\']+$').intersection(element):
-                        #print element
-
-parse_users(soup)
-
-file_users.close()
-print users
-    #print '*****' + str(coses)
+#print '*****' + str(coses)
 
 '''
 print soup
