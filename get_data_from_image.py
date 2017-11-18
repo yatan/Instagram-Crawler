df812a5816fe7a3c5ca743d781391ca34e111ff2
yatan <yatan87@gmail.com>
2017-11-19 00:39:46 +0100

    get_data script

--- get_data_from_image.py
+++ get_data_from_image.py
@@ -1,18 +1,34 @@
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
-import os
+import os, sqlite3
 
-url1 = "https://scontent-mad1-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22500209_124269068257773_2270043748131930112_n.jpg"
+db = sqlite3.connect('./web/data/crawler')
 
-app = ClarifaiApp(api_key='f607cf42c8b64ff499931bdf6a5a53a6')
-# get the general model
-model = app.models.get("general-v1.3")
-# predict with the model
-response = model.predict_by_url(url=url1)
 
-concepts = response['outputs'][0]['data']['concepts']
-for concept in concepts:
-    print(concept['name'], concept['value'])
+cursor = db.cursor()
+cursor.execute('SELECT * FROM links')
+for row in cursor:
+    # row['name'] returns the name column in the query, row['email'] returns email column.
+    print "Geting: " + str(row[0]) + " " + row[2]
+
+    #print('Getting info link id {0} = {1}'.format(row["id"], row["link"]))
+
+    url1 = row[2]
+
+    app = ClarifaiApp(api_key='f607cf42c8b64ff499931bdf6a5a53a6')
+    # get the general model
+    model = app.models.get("general-v1.3")
+    # predict with the model
+    response = model.predict_by_url(url=url1)
+
+    concepts = response['outputs'][0]['data']['concepts']
+    for concept in concepts:
+        print(concept['name'], concept['value'])
+
+db.close()
+
+
+
 
     
 
