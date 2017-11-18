16671021a3ab4eabe8c24a34f770177dbfa3a859
yatan <yatan87@gmail.com>
2017-11-18 18:10:04 +0100

    concepts

--- get_data.py
+++ get_data.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import selenium.webdriver as webdriver
 
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+
 url = 'http://instagram.com/umnpics/'
 driver = webdriver.Chrome()
 driver.set_window_size(300, 300) # set window size
@@ -14,3 +17,15 @@ for foto in soup.findAll('img'):
 
 
 driver.close()
+
+app = ClarifaiApp(api_key='f607cf42c8b64ff499931bdf6a5a53a6')
+
+# get the general model
+model = app.models.get("general-v1.3")
+
+# predict with the model
+response = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+
+concepts = response['outputs'][0]['data']['concepts']
+for concept in concepts:
+    print(concept['name'], concept['value'])
