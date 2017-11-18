fc7f011f2da4481432586ecdea51550988c89224
yatan <yatan87@gmail.com>
2017-11-18 15:05:55 +0100

    save file users

--- crawler.py
+++ crawler.py
@@ -1,9 +1,15 @@
 import urllib2
+import re
 from bs4 import BeautifulSoup
 #from BeautifulSoup import BeautifulSoup
-response = urllib2.urlopen('https://www.instagram.com/twittersoz/')
+active_user = "twittersoz"
+response = urllib2.urlopen('https://www.instagram.com/' + active_user)
 html = response.read()
 soup = BeautifulSoup(html, "html.parser")
+
+users = []
+file_users = open("users.txt","w") 
+
 
 for coses in soup.find_all('a'):
     #print coses.get('a')
@@ -14,9 +20,14 @@ for coses in soup.find_all('a'):
             tag2 = tag.split(' ')
             for element in tag2:
                 if "@" in element:
-                    print element
+                    users.append(element.replace("@", ""))
+                    file_users.write(element.replace("@", "") + "\n")
+                    #if re.match(r'.*[\%\$\^\*\@\!\_\-\(\)\:\;\'\"\{\}\[\]].*', element):
+                    #if set('[~!@#$%^&*()_+{}":;\']+$').intersection(element):
+                    #print element
 
-
+file_users.close()
+print users
     #print '*****' + str(coses)
 
 '''
