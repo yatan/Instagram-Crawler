68064b85e46050a24f0fe418c36c6fdc8a0f6615
yatan <yatan87@gmail.com>
2017-11-18 18:50:10 +0100

    links_txt

--- get_images_from_profile.py
+++ get_images_from_profile.py
@@ -1,17 +1,35 @@
 from bs4 import BeautifulSoup
+import os
 import selenium.webdriver as webdriver
 
-url = 'http://instagram.com/umnpics/'
-driver = webdriver.Chrome()
-driver.set_window_size(300, 300) # set window size
-driver.get(url)
+def get_img_url(url):
+    direccions = []
+
+    url = 'http://instagram.com/' + url
+    driver = webdriver.Chrome()
+    driver.set_window_size(300, 300) # set window size
+    driver.get(url)
 
 
-soup = BeautifulSoup(driver.page_source, "html.parser")
+    soup = BeautifulSoup(driver.page_source, "html.parser")
 
-for foto in soup.findAll('img'):
-    print foto.get('alt', '')
-    print foto.get('src', '')
+    for foto in soup.findAll('img'):
+        #print foto.get('alt', '')
+        direccions.append( foto.get('src', '') )
+
+    driver.close()
+    print direccions
+    return direccions
+
+for filename in os.listdir('.'):
+    # print filenames of dir
+    if ("users_" in filename):
+        user = filename.split("users_")[1]
+        userbo = user.split(".txt")[0]
+        file_links = open("links_" + userbo + ".txt","w") 
+        links = get_img_url(userbo)
+        for items in links:
+            file_links.write(items + "\n")
+        file_links.close()
 
 
-driver.close()
