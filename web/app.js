cd01f1a234df0b2ee637d327e38aa9e9d28536a9
yatan <yatan87@gmail.com>
2017-11-18 23:10:17 +0100

    get_links

--- web/app.js
+++ web/app.js
@@ -6,35 +6,51 @@ var path = require('path');
 
 var usuaris = [];
 
-db.serialize(function() {
+db.serialize(function () {
 
-  db.each('SELECT * FROM users', function(err, row) {
+  db.each('SELECT * FROM users', function (err, row) {
     usuaris.push(row.nickname);
-    // console.log(row.nickname);
   });
 
 });
 
-db.close();
+
+
+function get_links(username, callback) {
+  
+  db.serialize(function () {
+    var links = [];
+    db.each("SELECT * FROM links WHERE nickname=?",username , function (err, row) {
+      links.push(row.link);
+/*    console.log(JSON.stringify(links));
+      return(JSON.stringify(links)); */
+    });
+
+    console.log(links);
+    return JSON.stringify(links);
+  });
+}
+
 
 
 app.get('/', function (req, res) {
-  if ( typeof req.query.name !== 'undefined' && req.query.name )
-  {
+  if (typeof req.query.name !== 'undefined' && req.query.name) {
     let usuari = req.query.name;
+    var llista_users = get_links2(usuari);
+    console.log(get_links(usuari));
     //console.log(req.query.name);
-    res.render('index', { 
+    res.render('index', {
       title: 'Links from user',
       user: usuari,
-      usuarios : usuaris
-      });
+      links: llista_users,
+      usuarios: usuaris
+    });
   }
-  else
-  {
-    res.render('index', { 
+  else {
+    res.render('index', {
       title: 'Userlist',
-      usuarios : usuaris
-      });
+      usuarios: usuaris
+    });
   }
 });
 
@@ -44,3 +60,5 @@ app.set('view engine', 'jade');
 app.listen(3000, function () {
   console.log('Example app listening on http://localhost:3000 !');
 });
+
+//db.close();
