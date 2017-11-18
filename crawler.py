07f2b3ecdf0fcc9567be249d1182d3da9d7cac74
yatan <yatan87@gmail.com>
2017-11-18 15:18:29 +0100

    argv user

--- crawler.py
+++ crawler.py
@@ -1,8 +1,14 @@
 import urllib2
+import sys
 import re
 from bs4 import BeautifulSoup
 #from BeautifulSoup import BeautifulSoup
+
+#GET user value from argument or using 'default'
 active_user = "twittersoz"
+if len(sys.argv) == 2:
+    active_user = sys.argv[1]
+
 response = urllib2.urlopen('https://www.instagram.com/' + active_user)
 html = response.read()
 soup = BeautifulSoup(html, "html.parser")
@@ -11,20 +17,25 @@ users = []
 file_users = open("users.txt","w") 
 
 
-for coses in soup.find_all('a'):
-    #print coses.get('a')
-    #nicks = coses.find_all('a')
-    conv = str(coses)
-    for tag in conv.split('>'):
-        if "@" in tag:
-            tag2 = tag.split(' ')
-            for element in tag2:
-                if "@" in element:
-                    users.append(element.replace("@", ""))
-                    file_users.write(element.replace("@", "") + "\n")
-                    #if re.match(r'.*[\%\$\^\*\@\!\_\-\(\)\:\;\'\"\{\}\[\]].*', element):
-                    #if set('[~!@#$%^&*()_+{}":;\']+$').intersection(element):
-                    #print element
+def parse_users(soup):
+    for coses in soup.find_all('a'):
+        #print coses.get('a')
+        #nicks = coses.find_all('a')
+        conv = str(coses)
+        for tag in conv.split('>'):
+            if "@" in tag:
+                tag2 = tag.split(' ')
+                for element in tag2:
+                    if "@" in element:
+                        usuari = element.replace("@", "")
+                        if usuari not in users:
+                            users.append(usuari)
+                            file_users.write(usuari + "\n")
+                        #if re.match(r'.*[\%\$\^\*\@\!\_\-\(\)\:\;\'\"\{\}\[\]].*', element):
+                        #if set('[~!@#$%^&*()_+{}":;\']+$').intersection(element):
+                        #print element
+
+parse_users(soup)
 
 file_users.close()
 print users
