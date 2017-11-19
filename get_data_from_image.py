14cf25dc0d8d05373314535699309448e6aaeecc
yatan <yatan87@gmail.com>
2017-11-19 01:17:16 +0100

    get data image

--- get_data_from_image.py
+++ get_data_from_image.py
@@ -29,16 +29,23 @@ cursor = db.cursor()
 cursor.execute('SELECT * FROM links')
 for row in cursor:
     # row['name'] returns the name column in the query, row['email'] returns email column.
-    print "Geting info id: " + str(row[0]) + " url: " + row[2]
+    photo_id = str(row[0])
+    if not check_existed_link_id(photo_id):
+        print "Geting info id: " + photo_id + " url: " + row[2]
 
-    #print('Getting info link id {0} = {1}'.format(row["id"], row["link"]))
-    # Get API info to DB
-    # for concept in concepts:
-    #    print(concept['name'], concept['value'])
-    concepts = get_API(row[2])
-    for concept in concepts:
-        print(concept['name'], concept['value'])
-
+        #print('Getting info link id {0} = {1}'.format(row["id"], row["link"]))
+        # Get API info to DB
+        # for concept in concepts:
+        #    print(concept['name'], concept['value'])
+        concepts = get_API(row[2])
+        cursor2 = db.cursor()
+        for concept in concepts:
+            cursor2.execute('INSERT INTO data(link_id, type, posible) VALUES(?,?,?)', ((str(row[0]), concept['name'], str(concept['value'])) ))
+            print "Adding id: " + str(row[0]) + " " + concept['name'] + ":" + str(concept['value']) + " to DB."
+        # Write Changes
+        db.commit()
+    else:
+        print photo_id + " ja existeix."
 
 db.close()
 
