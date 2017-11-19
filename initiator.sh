bc808e75ef90b84ff4f076f4105f03632beb23b2
yatan <yatan87@gmail.com>
2017-11-19 06:07:12 +0100

    created initiator and readme

---
+++ initiator.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "************************"
+echo "CRAWLER HACKEPS" 
+echo "************************"
+
+echo "Menu:"
+echo "************************"
+echo "1 - Inici crawler.py sense especificar seed"
+echo "2 - Inici crawler.py especificant seed"
+echo "************************"
+echo "3 - Obtenir imatges dels perfils - get_images_from_profile.py"
+echo "************************"
+echo "4 - Exget_images_from_profile.pyportar users --> DB  (export_users_db.py)"
+echo "5 - Exportar links --> DB (export_links_db.py)"
+echo "************************"
+echo "6 - Obtenir informació de les imatges via Clarifi API (get_data_from_image.py)"
+read menu
+
+case "$menu" in
+1)
+python crawler.py
+;;
+2)
+python crawler.py alpersaldiran
+;;
+3)
+python get_images_from_profile.py
+;;
+4)
+python export_users_db.py
+;;
+5)
+python export_links_db.py
+;;
+6)
+python get_data_from_image.py
+;;
+*) echo "Opcio incorrecta"
+   ;;
+esac
