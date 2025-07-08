29e22d396a72718e6e0e64a7ab66097da9e94064
Fran Romero <yatan87@gmail.com>
2025-07-09 00:46:28 +0200

    Upgrade to python3

--- crawler.py
+++ crawler.py
@@ -1,11 +1,12 @@
-import urllib2
-import sys, subprocess, time
-# import re
-from threading import Thread
+# Compatible con Python 3
+import sys, subprocess, time, os
+from threading import Thread, Lock
 from bs4 import BeautifulSoup
+import urllib.request
 
 max_crawl = 5
 actual_crawl = 0
+actual_crawl_lock = Lock()
 
 
 # GET user value from argument or using 'default'
@@ -15,110 +16,81 @@ if len(sys.argv) == 2:
 
 
 class Crawler(Thread):
-    users = []
-    scanned_users = []
-    active_user = ""
-
     def __init__(self, active_user):
         Thread.__init__(self)
         self.active_user = active_user
+        self.users = []
+        self.scanned_users = []
 
     # Thread worker for active_user
-    def run (self):
-        print "Analizyng user: " + active_user
-
-        # Parse active user
-        response = urllib2.urlopen('https://www.instagram.com/' + active_user)
-        html = response.read()
-        soup = BeautifulSoup(html, "html.parser")
-        # Parse users
-        self.parse_users(soup)
+    def run(self):
+        print("Analizyng user: " + self.active_user)
+        try:
+            response = urllib.request.urlopen('https://www.instagram.com/' + self.active_user)
+            html = response.read()
+            soup = BeautifulSoup(html, "html.parser")
+            self.parse_users(soup)
+        except Exception as e:
+            print(f"Error fetching user {self.active_user}: {e}")
 
     def check_exist(self, username):
-        file_users_analized = open("analized_users.txt","r")
-        loglist = file_users_analized.readlines()
-        file_users_analized.close()
+        if not os.path.exists("analized_users.txt"):
+            return False
+        with open("analized_users.txt", "r") as file_users_analized:
+            loglist = file_users_analized.readlines()
         found = False
         for line in loglist:
             if str(username) in line:
                 found = True
         return found
 
-    def parse_users(self, soup):
 
+    def parse_users(self, soup):
         global actual_crawl
         global max_crawl
+        global actual_crawl_lock
 
-        file_users = open("users_" + active_user + ".txt","w") 
-        for coses in soup.find_all('a'):
-            #print coses.get('a')
-            #nicks = coses.find_all('a')
-            conv = str(coses)
-            for tag in conv.split('>'):
-                if "@" in tag:
-                    tag2 = tag.split(' ')
-                    for element in tag2:
-                        # Detecta si es usuari
-                        if "@" in element:
-                            usuari = element.replace("@", "")
-                            if usuari not in self.users:
-                                self.users.append(usuari)
-                                file_users.write(usuari + "\n")
-                            #if re.match(r'.*[\%\$\^\*\@\!\_\-\(\)\:\;\'\"\{\}\[\]].*', element):
-                            #if set('[~!@#$%^&*()_+{}":;\']+$').intersection(element):
-                            #print element
-        file_users.close()
-        #Add user to users analized
-        file_users_analized = open("analized_users.txt","a")
-        file_users_analized.write(self.active_user + "\n")
-        file_users_analized.close() 
-        actual_crawl -= 1
+        with open("users_" + self.active_user + ".txt", "w") as file_users:
+            for coses in soup.find_all('a'):
+                conv = str(coses)
+                for tag in conv.split('>'):
+                    if "@" in tag:
+                        tag2 = tag.split(' ')
+                        for element in tag2:
+                            if "@" in element:
+                                usuari = element.replace("@", "")
+                                if usuari not in self.users:
+                                    self.users.append(usuari)
+                                    file_users.write(usuari + "\n")
 
+        with open("analized_users.txt", "a") as file_users_analized:
+            file_users_analized.write(self.active_user + "\n")
 
+        with actual_crawl_lock:
+            actual_crawl -= 1
 
         for usuari in self.users:
-            if actual_crawl > max_crawl:
-                # Add a little time
+            with actual_crawl_lock:
+                if actual_crawl > max_crawl:
+                    need_sleep = True
+                else:
+                    need_sleep = False
+            if need_sleep:
                 time.sleep(5)
             else:
-                print "worker " + usuari
-                if ( self.check_exist(usuari) == False):
-                    actual_crawl += 1
-                    self.child = subprocess.Popen([sys.executable, './crawler.py', usuari])
+                print("worker " + usuari)
+                if not self.check_exist(usuari):
+                    with actual_crawl_lock:
+                        actual_crawl += 1
+                    self.child = subprocess.Popen([sys.executable, os.path.abspath(__file__), usuari])
 
-
-
-    def join( self ):
-        Thread.join( self )
+    def join(self):
+        Thread.join(self)
         return self.users
 
-
-
-
 # Llista threads actius
-e = Crawler(active_user)
-e.start()
-print e.join()
+if __name__ == "__main__":
+    e = Crawler(active_user)
+    e.start()
+    print(e.join())
 
-
-#print '*****' + str(coses)
-
-'''
-print soup
-for line in soup.split('\n'):
-    for line2 in line.split('@'):
-        print line2'''
-
-'''for link in soup.find_all('link'):
-    print link
-    #print(link.get('href'))'''
-
-
-'''
-for line in html.split('\n'):
-    for line2 in line.split('@'):
-        print [pos for pos, char in enumerate('@') if char == '@']'''
-
-
-    #res = [k for k in line if '@' in k]
-    #print res
